8222fa77099085a3ef6fe6b74eef6cf30b7e228d
Duban Velez <duban5555@gmail.com>
2026-09-03 23:55:00 -0500

    feat: add Docker configuration and deployment workflow with Nginx setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  goAgendaApiUrl: 'https://api.goagenda.online',
+  supabaseUrl: 'https://ghwkiuwcovrmpztamjgz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_UYyewNFK6XwIwTjyk6stZA_sfb5KXtX'
+};
